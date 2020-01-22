54c3a3eb7903529e7665e7015fbbe92edc461cd2
Bhargava Krishna Prabhakara <TGL145@techmindsindia.local>
2020-01-22 19:48:26 +0530

    demo_date

--- demo_date.js
+++ demo_date.js
@@ -9,3 +9,14 @@ function test(paramx){
 
     
 } 
+
+/**
+ * Description: javascript Dates
+ */
+
+function test5(Date){
+    var x = new Date(2018, 11, 24, 10, 33, 30, 0)
+    return x
+    
+} 
+
