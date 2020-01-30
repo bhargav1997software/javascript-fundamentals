0829b2c5d9774a41bec2ac9622b1337a7fae8f85
bhargavsoftware1997 <TGL145@techmindsindia.local>
2020-01-30 16:25:11 +0530

    new commit

--- demo_datetask.js
+++ demo_datetask.js
@@ -2,10 +2,12 @@
  * Description: javascript Dates
  */
 
+function monthDiff(dt1, dt2) {
 
-function monthDif(dateFrom, dateTo) {
-  
-    return dateTo.getMonth() - dateFrom.getMonth() +
-      (12 * (dateTo.getFullYear() - dateFrom.getFullYear()))
-   }
-  
+   var years = dt2.getFullYear() - dt1.getFullYear();
+   var Month = (dt2.getMonth() + 1) - (dt1.getMonth() + 1);
+   var diffMonth = years * 12 + Month;
+   return diffMonth;
+
+}
+
