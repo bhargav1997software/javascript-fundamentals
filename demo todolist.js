7bccc616a42f0c6085a3f871a8e886d0c69a5eb9
bhargavsoftware1997 <TGL145@techmindsindia.local>
2020-02-10 13:07:48 +0530

    new commit

--- demo todolist.js
+++ demo todolist.js
@@ -1,4 +1,4 @@
-
+// Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
@@ -8,7 +8,7 @@ for (i = 0; i < myNodelist.length; i++) {
   span.appendChild(txt);
   myNodelist[i].appendChild(span);
 }
-
+// Click on a close button to hide the current list item
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
@@ -17,7 +17,7 @@ for (i = 0; i < close.length; i++) {
     div.style.display = "none";
   }
 }
-
+// Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('ul');
 list.addEventListener('click', function(ev) {
   if (ev.target.tagName === 'LI') {
@@ -25,13 +25,14 @@ list.addEventListener('click', function(ev) {
   }
 }, false);
 
+// Create a new list item when clicking on the "Add" button
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
   var t = document.createTextNode(inputValue);
   li.appendChild(t);
   if (inputValue === '') {
-    alert("You must write something to add to your to do list!");
+    alert("You must write something!");
   } else {
     document.getElementById("myUL").appendChild(li);
   }
@@ -50,3 +51,4 @@ function newElement() {
     }
   }
 }
+
